f94aeb950a3a2db000f08ec60d27aeeb01d20bcb
sorocode <tudgus010707@naver.com>
2025-05-17 20:51:39 +0900

    🐛  .env 환경변수로 변경

--- lib/locator.dart
+++ lib/locator.dart
@@ -7,12 +7,12 @@ void setupLocator() {
   getIt.registerLazySingleton<Dio>(() {
     final dio = Dio();
 
-    // 공통 설정 (선택)
+    // 공통 설정
     dio.options.baseUrl = const String.fromEnvironment('API_BASE_URL',
-        defaultValue: 'http://localhost:8080');
+        defaultValue: 'http://10.0.2.2:8080');
     dio.options.connectTimeout = const Duration(seconds: 10);
     dio.options.receiveTimeout = const Duration(seconds: 10);
-
+    dio.interceptors.add(LogInterceptor(requestBody: true, responseBody: true));
     return dio;
   });
 }
