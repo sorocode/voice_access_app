ad6a792a5fe90ee9391dab85f574c792f2ad7c90
sorocode <tudgus010707@naver.com>
2025-06-01 12:05:21 +0900

    🐛 api 응답시간 연장

--- lib/locator.dart
+++ lib/locator.dart
@@ -11,7 +11,8 @@ void setupLocator() {
     // 공통 설정
     dio.options.baseUrl = dotenv.env['API_BASE_URL'] ?? 'http://10.0.2.2:8080';
     dio.options.connectTimeout = const Duration(seconds: 10);
-    dio.options.receiveTimeout = const Duration(seconds: 10);
+    dio.options.receiveTimeout = const Duration(seconds: 60);
+    dio.options.sendTimeout = const Duration(seconds: 30);
     dio.interceptors.add(LogInterceptor(requestBody: true, responseBody: true));
     return dio;
   });
